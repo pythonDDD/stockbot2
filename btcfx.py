f66a34bad808cc5a51bbffb287197e501b59a4ec
pythonDDD <ange_void@yahoo.jp>
2020-05-16 20:22:39 +0900

    make it better

--- btcfx.py
+++ btcfx.py
@@ -1,4 +1,7 @@
+import os
 import pybitflyer
+import requests
+
 #環境変数取得
 api_key = os.environ["api_key"]
 api_secret = os.environ["api_secret"]
@@ -8,8 +11,6 @@ api = pybitflyer.API(api_key="api_key", api_secret="api_secret")
 
 data = api.getcollateral();
 
-
-import requests
 
 # bitflyer で取得
 res = requests.get('https://api.bitflyer.jp/v1/ticker?product_code=FX_BTC_JPY')
