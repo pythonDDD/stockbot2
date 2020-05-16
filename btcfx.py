86a88e7bc31c0564f26e11a636134516ce98f811
pythonDDD <ange_void@yahoo.jp>
2020-05-16 21:33:52 +0900

    make it better

--- btcfx.py
+++ btcfx.py
@@ -1,15 +1,7 @@
-import os
-import pybitflyer
+
+
 import requests
 
-#環境変数取得
-api_key = os.environ["api_key"]
-api_secret = os.environ["api_secret"]
-
-
-api = pybitflyer.API(api_key="api_key", api_secret="api_secret")
-
-data = api.getcollateral();
 
 
 # bitflyer で取得
